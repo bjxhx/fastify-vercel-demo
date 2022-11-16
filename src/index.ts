4bdc3d5c0ee44003a45fe1640d851a87876c409a
gitee_user <491919843@qq.com>
2022-11-16 09:25:17 +0800

    add image

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import type { FastifyReply, FastifyRequest } from "fastify";
-
+import axios from "axios";
 export default async function routes(fastify, options) {
   fastify.get("/", async (request: FastifyRequest, reply: FastifyReply) => {
     return { hello: "world" };
@@ -7,5 +7,10 @@ export default async function routes(fastify, options) {
 
   fastify.get("/hi", async (request: FastifyRequest, reply: FastifyReply) => {
     return { hi: "zakiego" };
+  });
+
+  fastify.get("/image", async (request: FastifyRequest, reply: FastifyReply) => {
+    request.query
+    return request.query;
   });
 }
